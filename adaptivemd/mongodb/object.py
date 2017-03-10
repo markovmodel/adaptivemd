f2cfe8524eb5362f43768a17bec763e6335e8076
Jan-Hendrik Prinz <jan.prinz@choderalab.org>
2017-03-10 17:22:36 +0100

    autofix objects

--- adaptivemd/mongodb/object.py
+++ adaptivemd/mongodb/object.py
@@ -622,20 +622,21 @@ class ObjectStore(StorableMixin):
             the loaded object
         """
 
-        self.check_size()
 
         if type(idx) is str:
             idx = int(UUID(self._document.find_one({'name': idx})['_id']))
 
         if type(idx) is long:
             if idx not in self.index:
-                raise ValueError(
-                    'str %s not found in storage' % idx)
+                self.check_size()
+                if idx not in self.index:
+                    raise ValueError(
+                        'str %s not found in storage' % idx)
 
-        elif type(idx) is not int:
+        else:
             raise ValueError((
                 'indices of type "%s" are not allowed in named storage '
-                '(only str and int)') % type(idx).__name__
+                '(only str and long)') % type(idx).__name__
             )
 
         # if it is in the cache, return it
