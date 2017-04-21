587869d2b7e8851448332042e8d7ff3ff3e90522
Martin K. Scherer <m.scherer@fu-berlin.de>
2017-04-21 18:44:00 +0200

    import guard

--- adaptivemd/scripts/_run_.py
+++ adaptivemd/scripts/_run_.py
@@ -33,24 +33,24 @@ This executes a function remotely and expects an `input.json` file to contain
 a reference to the function and module as well as arguments and keyword arguments
 """
 
+if __name__ == '__main__':
+    import importlib
+    from adaptivemd.mongodb import ObjectJSON
+    import os
+    import sys
+    sys.path.insert(0, os.path.abspath('.'))
 
-import importlib
-from adaptivemd.mongodb import ObjectJSON
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+    simplifier = ObjectJSON()
 
-simplifier = ObjectJSON()
+    with open('input.json', 'r') as f:
+        data = simplifier.from_json(f.read())
 
-with open('input.json', 'r') as f:
-    data = simplifier.from_json(f.read())
+    parts = data['function'].split('.')
 
-parts = data['function'].split('.')
+    fnc = importlib.import_module('.'.join(parts[:-1]))
+    fnc = getattr(fnc, parts[-1])
 
-fnc = importlib.import_module('.'.join(parts[:-1]))
-fnc = getattr(fnc, parts[-1])
+    result = fnc(**data['kwargs'])
 
-result = fnc(**data['kwargs'])
-
-with open('output.json', 'w') as f:
-    f.write(simplifier.to_json(result))
+    with open('output.json', 'w') as f:
+        f.write(simplifier.to_json(result))
