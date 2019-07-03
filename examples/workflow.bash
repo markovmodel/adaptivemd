1cf09301814f73b1d06058402ddfb8883de9db06
jrossyra <jrossyra@gmail.com>
2019-07-03 15:11:42 -0400

    [examples] workflow script updated

--- examples/workflow.bash
+++ examples/workflow.bash
@@ -1,38 +1,65 @@
 #!/bin/bash
 
+# Folder containing OpenMM Simulation XML Files
+#  - must be discoverable, i.e. in 'examples/files/' or
+#    user-specified directory in env variable "$ADMD_MDSYSTEMS"
+SYS="alanine"
 
-source $ADMD_RUNTIME/wf_funcs.sh
+# Walltime for LRMS job
+MINS="50"
 
-SYSNAME="alanine"
-
-MINUTES=50
-#EXECFLAG="--rp"
-#EXECFLAG=/lustre/atlas/proj-shared/bip149/mongodb/pG.db
+# relative or absolute path to DB parent folder
+#  - optional DB port for head node to avoid
+#    congestionon default port
 EXECFLAG="mongo:43224"
-PROJ="pG"
-MFREQ=10000
-PFREQ=2000
-PLATFORM=CUDA
 
-N1=50
-P1i=1000000
-P1t=8000000
+# Name for AdaptiveMD Project instance
+PROJ="alanine"
 
-# Need to fill out these options to define a workflow
-# <admd_command> $ROUNDNUMBER $PROJNAME <workloadtype> $NTRAJ $NSTEPS $TSTEPS $AFTERNTRAJS $MODELTRAJLENGTH $SAMPLINGFUNCTION $MINUTES $EXECFLAG
-# TODO define clearly each argument
+# Name of AdaptiveMD sampling function to use
+SF="explore_macrostates"
 
-#------------------#
-#    Phase 1       #
-#------------------#
-checkpoint
-admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 explore_macrostates $MINUTES $EXECFLAG $SYSNAME $MFREQ $PFREQ $PLATFORM
-admd_workload 1 $PROJNAME trajs $N1 $P1CHUNK $P1LENGTH 0 0 explore_macrostates $MINUTES $EXECFLAG
-admd_workload 1 $PROJNAME trajs $N1 $P1CHUNK $P1LENGTH 0 0 explore_macrostates $MINUTES $EXECFLAG
-admd_workload 1 $PROJNAME trajs $N1 $P1CHUNK $P1LENGTH 0 0 explore_macrostates $MINUTES $EXECFLAG
-admd_workload 1 $PROJNAME trajs $N1 $P1CHUNK $P1LENGTH 0 0 explore_macrostates $MINUTES $EXECFLAG
-admd_workload 1 $PROJNAME trajs $N1 $P1CHUNK $P1LENGTH 0 0 explore_macrostates $MINUTES $EXECFLAG
-admd_workload 1 $PROJNAME trajs $N1 $P1CHUNK $P1LENGTH 0 0 explore_macrostates $MINUTES $EXECFLAG
-admd_workload 1 $PROJNAME trajs $N1 $P1CHUNK $P1LENGTH 0 0 explore_macrostates $MINUTES $EXECFLAG
-admd_workload 1 $PROJNAME model 1   0        0         0 0 explore_macrostates $MINUTES $EXECFLAG
+# Save frequencies for master (all atoms) trajectory
+# and protein-only trajectory. Save protein more
+# frequently to get more data for MSM building
+MFREQ="20000"
+PFREQ="4000"
+
+# OpenMM Simulation Platform
+PLATFORM="CUDA"
+
+# Number of replicates
+N1="2"
+
+# MD length per job in steps
+P1i="1000000"
+
+# Total MD length for full trajectory
+P1t="8000000"
+
+#------------------------------------------------------------------------------#
+#
+# Every workload line: need to fill out these options to define a workflow
+#  |   0             1           2            3           4      5    
+#  | <admd_command> roundnumber projectname workloadtype ntasks nsteps
+#  |
+#  |  6      7            8                9                10      11
+#  | tsteps afterntrajs minlengthformodel samplingfunction minutes execflag
+#
+# First workload line: needed to initialize AdaptiveMD Project for workflow
+#  |  12          13              14                15
+#  | systemname masterfrequency proteinfrequency simulationplatform
+#
+#------------------------------------------------------------------------------#
+
+admd_checkpoint
+#           0 1     2     3   4    5    6 7 8   9    10        11   12     13     14        15
+admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG $SYS $MFREQ $PFREQ $PLATFORM
+admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG
+admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG
+admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG
+admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG
+admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG
+admd_workload 1 $PROJ model 1   0    0    0 0 $SF $MINS $EXECFLAG
 # NEXT!
+
