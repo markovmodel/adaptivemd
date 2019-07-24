d8a31b0c2e6b1f35acced076b7210b876f0cca2b
jrossyra <jrossyra@gmail.com>
2019-07-23 22:53:12 -0400

    [examples] short added

--- examples/alanine/workflow.shortsims.bash
+++ examples/alanine/workflow.shortsims.bash
@@ -5,7 +5,7 @@
 #    user-specified directory in env variable "$ADMD_MDSYSTEMS"
 SYS="alanine"
 # Walltime for LRMS job
-MINS="50"
+MINS="15"
 # relative or absolute path to DB parent folder
 #  - optional DB port for head node to avoid
 #    congestionon default port
@@ -21,14 +21,14 @@ SF="explore_macrostates"
 # Save frequencies for master (all atoms) trajectory
 # and protein-only trajectory. Save protein more
 # frequently to get more data for MSM building
-MFREQ="20000"
-PFREQ="4000"
+MFREQ="200"
+PFREQ="40"
 # Number of replicates
 N1="2"
 # MD length per job in steps
-P1i="1000000"
+P1i="20000"
 # Total MD length for full trajectory
-P1t="8000000"
+P1t="40000"
 
 #------------------------------------------------------------------------------#
 #
@@ -47,6 +47,7 @@ P1t="8000000"
 
 admd_checkpoint
 #           0 1     2     3   4    5    6 7 8   9    10        11   12     13     14        15
+admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG $SYS $MFREQ $PFREQ $PLATFORM
 admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG $SYS $MFREQ $PFREQ $PLATFORM
 ## admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG
 ## admd_workload 1 $PROJ trajs $N1 $P1i $P1t 0 0 $SF $MINS $EXECFLAG
