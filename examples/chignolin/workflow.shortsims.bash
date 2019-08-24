48e816d47e3ae65b2d5b9aa5b92c0196a77acc43
jrossyra <jrossyra@gmail.com>
2019-08-24 11:36:58 -0400

    [examples] updates

--- examples/chignolin/workflow.shortsims.bash
+++ examples/chignolin/workflow.shortsims.bash
@@ -3,32 +3,32 @@
 # Folder containing OpenMM Simulation XML Files
 #  - must be discoverable, i.e. in 'examples/files/' or
 #    user-specified directory in env variable "$ADMD_MDSYSTEMS"
-SYS="alanine"
+SYS="chignolin"
 # Walltime for LRMS job
-MINS="15"
+MINS="45"
 # relative or absolute path to DB parent folder
 #  - optional DB port for head node to avoid
 #    congestionon default port
 #  - this argument is transformed as it travels
 #    down the pipe
-EXECFLAG="mongo:43224"
+EXECFLAG="mongo:23234"
 # OpenMM Simulation Platform
-PLATFORM="CPU"
+PLATFORM="CUDA"
 # Name for AdaptiveMD Project instance
-PROJ="alanine"
+PROJ="chignolin"
 # Name of AdaptiveMD sampling function to use
 SF="explore_macrostates"
 # Save frequencies for master (all atoms) trajectory
 # and protein-only trajectory. Save protein more
 # frequently to get more data for MSM building
-MFREQ="200"
-PFREQ="40"
+MFREQ="2500"
+PFREQ="500"
 # Number of replicates
-N="2"
+N="1"
 # MD length per job in steps
-Pi="20000"
+Pi="100000"
 # Total MD length for full trajectory
-Pt="40000"
+Pt="200000"
 
 #------------------------------------------------------------------------------#
 #
@@ -47,26 +47,29 @@ Pt="40000"
 
 admd_checkpoint
 
+# TODO test for behavior if first line is not a proper initializer
+
 #           0 1     2     3  4   5   6 7 8   9    10        11   12     13     14        15
-#admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG $SYS $MFREQ $PFREQ $PLATFORM
-#admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
-#admd_workload 1 $PROJ model 1  0   0   0 0 $SF $MINS $EXECFLAG
+admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG $SYS $MFREQ $PFREQ $PLATFORM
+admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
+admd_workload 1 $PROJ model 1  0   0   0 0 $SF $MINS $EXECFLAG
+
 admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 admd_workload 1 $PROJ model 1  0   0   0 0 $SF $MINS $EXECFLAG
-#
+
 #admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 #admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 #admd_workload 1 $PROJ model 1  0   0   0 0 $SF $MINS $EXECFLAG
-#
+
 #admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 #admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 #admd_workload 1 $PROJ model 1  0   0   0 0 $SF $MINS $EXECFLAG
-#
+
 #admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 #admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 #admd_workload 1 $PROJ model 1  0   0   0 0 $SF $MINS $EXECFLAG
-#
+
 #admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 #admd_workload 1 $PROJ trajs $N $Pi $Pt 0 0 $SF $MINS $EXECFLAG
 #admd_workload 1 $PROJ model 1  0   0   0 0 $SF $MINS $EXECFLAG
